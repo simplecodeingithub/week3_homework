6f4ef564663c8932028f30e3d5fd68df467f1aeb
Milena Davudova <mdavudova22@gmail.com>
2025-02-09 19:32:18 +0000

    EDIT: added comments

--- exercise11.py
+++ exercise11.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
+# splitting the string to make it a list
 Belgium_list=Belgium.split(",")
+# printing - for the length of belgium string, followed by new line, followed by the string with , replaced by :, newline
+# followed by the sum of Belgium and Brussels population made into integers
 print("-"*len(Belgium), "\n", Belgium.replace(",", ":"), "\n", int(Belgium_list[1])+int(Belgium_list[3]), "\n")
