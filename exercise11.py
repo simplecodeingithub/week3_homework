3e1dd14716bbc562d0eb4573257ba2a522870baf
Milena Davudova <mdavudova22@gmail.com>
2025-02-09 17:34:59 +0000

    ADD: milena's homework

---
+++ exercise11.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+
+Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
+print("-"*len(Belgium), Belgium.replace(",", ":"), 11763650+196828)
