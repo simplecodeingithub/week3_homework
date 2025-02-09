6902ec6450d2f06a7d2d71b6d1f51cf0c96daef3
Milena Davudova <mdavudova22@gmail.com>
2025-02-09 19:29:12 +0000

    EDIT:changed the print statement

--- exercise11.py
+++ exercise11.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
 
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
-print("-"*len(Belgium), Belgium.replace(",", ":"), 11763650+196828)
+Belgium_list=Belgium.split(",")
+print("-"*len(Belgium), "\n", Belgium.replace(",", ":"), "\n", int(Belgium_list[1])+int(Belgium_list[3]), "\n")
