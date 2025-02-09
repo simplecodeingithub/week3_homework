522c3ce0d18ea90e42c6c5cd09d485f507160958
Serena Gallinotti <serena.gallinotti@gmail.com>
2025-02-09 18:21:22 +0000

    UPDATED file

--- ex11_starter.py
+++ ex11_starter.py
@@ -3,13 +3,13 @@
 from xml.sax.handler import property_interning_dict
 
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
-
-
-print('the lenght of string is',len(Belgium))
-print(Belgium.replace(Belgium, '-'* 81))
+# 1 example
+print('the length of string is',len(Belgium))
+#  2 example
+print('-' *len(Belgium))
 
 Belgium_list = Belgium.split(',')
 Belgium_list[1]= int(Belgium_list[1])
 Belgium_list[3]= int(Belgium_list[3])
-print('the total population of Belgium + its capital is', Belgium_list[1]+ Belgium_list[3])
+print(Belgium.replace(Belgium, '-'* 81),'\n',('the total population of Belgium + its capital is', Belgium_list[1]+ Belgium_list[3]))
 
